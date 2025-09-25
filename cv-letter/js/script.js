0050b06ba7ad2525e9110fccaba53d902730066a
al-burtsev <burtsev.aleksandr9@gmail.com>
2025-09-25 19:12:39 +0300

    rework the content appearing

--- cv-letter/js/script.js
+++ cv-letter/js/script.js
@@ -1,80 +1,144 @@
-gsap.registerPlugin(ScrollTrigger, SplitText)
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, SplitText)
 
-const start = document.getElementById('start');
-const noAnswers = document.querySelectorAll('.no-answer');
-const show = document.querySelectorAll('.showImg');
-const showNext = document.querySelectorAll('.showNextImg');
-const goFinish = document.querySelectorAll('.showLastImg');
-const noModal = document.getElementById('noModal');
-const yesModal = document.getElementById('yesModal');
-const yes = document.getElementById('accept');
+const startSection = document.getElementById('start-section');
+const startBtn = document.getElementById('start-chat');
+const cancelAnswers = document.querySelectorAll('.cancel-btn');
+const chat = document.querySelector('.chat');
+const messages = document.querySelectorAll('.message');
+const attachImges = document.querySelectorAll('.attachment__img');
+const cancelModal = document.getElementById('cancelModal');
+const acceptModal = document.getElementById('acceptModal');
+const acceptBtn = document.getElementById('acceptBtn');
 
-start.addEventListener('click', () => {
-    document.querySelector('div[data-target="phrase-1"]').classList.remove('hide');
-    setTimeout(() => {
-        document.querySelector('div[data-target="phrase-2"]').classList.remove('hide');
-    }, 5000);
+const startTl = gsap.timeline();
+const decisionTl = gsap.timeline({ paused: true });
+const arrowTl = gsap.timeline({ paused: true });
+
+decisionTl.to('.decision', {
+    autoAlpha: 1,
+    delay: 2,
+    duration: 1,
+    ease: 'sine.in'
+})
+
+arrowTl
+    .to('.arrow-down', {
+        autoAlpha: 1,
+        ease: 'sign.in'
+    })
+    .to('.arrow-down', {
+        y: 10,
+        repeat: -1,
+        yoyo: true,
+        duration: 1,
+        ease: 'sign.in'
+    })
+
+const q = gsap.utils.selector(startSection);
+const firstTalk = q('.talk');
+const notice = q('.notice');
+const choice = q('.choice');
+
+gsap.set(firstTalk, {
+    transformOrigin: 'top left'
 });
 
-show.forEach((item) => {
-    item.addEventListener('click', () => {
-        document.getElementById("imgFirst").classList.remove('none');
-        document.getElementById("hide-1").classList.add('hide');
-        setTimeout(() => {
-            document.querySelector('div[data-target="phrase-3"]').classList.remove('hide');
-            setTimeout(() => {
-                document.querySelector('div[data-target="phrase-4"]').classList.remove('hide');
-            }, 5000);
-        }, 3000);
+startTl
+    .from(firstTalk, {
+        scale: 0,
+        opacity: 0,
+        duration: 0.7,
+        ease: 'power4.out',
+    })
+    .from(notice, {
+        y: -20,
+        opacity: 0,
+        duration: 1,
+        ease: 'back.in',
+    }, '+=1.5')
+    .fromTo(choice, {
+        opacity: 0,
+        y: -25,
+    }, {
+        y: 0,
+        scale: 1,
+        opacity: 1,
+        duration: 0.6,
+        ease: "back.inOut",
+    }, '+=1')
+
+messages.forEach((message, index) => {
+    const talk = message.querySelector('.talk');
+    const attachment = message.querySelector('.attachment');
+
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: message,
+            start: `top ${10 + 10 * index}%`,
+            end: 'bottom top',
+            toggleActions: 'play none none none',
+        },
+        onComplete: () => {
+            if (message.id === 'last-mess') {
+                decisionTl.play();
+            }
+
+            arrowTl.play();
+        }
+    });
+
+    tl.fromTo(talk, {
+        scale: 0,
+        opacity: 0,
+        transformOrigin: 'top left'
+    }, {
+        scale: 1,
+        opacity: 1,
+        duration: 0.7,
+        ease: 'power4.out',
+    });
+
+    if (attachment) {
+        tl.fromTo(attachment, {
+            scale: 0,
+            opacity: 0,
+            transformOrigin: 'top left'
+        }, {
+            scale: 1,
+            opacity: 1,
+            duration: 0.8,
+            ease: 'power4.out',
+        }, '+=0.3');
+    }
+});
+
+attachImges.forEach((elem) => {
+    elem.addEventListener('click', (e) => {
+        const picture = e.target.closest('.attachment__img');
+        const photo = picture.nextElementSibling;
+
+        const picTl = gsap.timeline();
+
+        picTl
+            .to(picture, {
+                opacity: 0,
+                duration: 1.2,
+                ease: 'power4.out',
+                onComplete: () => {
+                    gsap.set(picture, { height: 0, })
+                    gsap.set(photo, { display: 'flex', opacity: 0, })
+                }
+            })
+            .to(photo, {
+                autoAlpha: 1,
+                duration: 0.6,
+                ease: 'power4.out',
+            })
     });
 });
 
-showNext.forEach((item) => {
-    item.addEventListener('click', () => {
-        document.getElementById("imgSecond").classList.remove('none');
-        document.getElementById("hide-2").classList.add('hide');
-        setTimeout(() => {
-            document.querySelector('div[data-target="phrase-5"]').classList.remove('hide');
-            setTimeout(() => {
-                document.querySelector('div[data-target="phrase-6"]').classList.remove('hide');
-                setTimeout(() => {
-                    document.querySelector('div[data-target="phrase-7"]').classList.remove('hide');
-                }, 5000);
-            }, 8000);
-        }, 2000);
-    });
-});
-
-goFinish.forEach((item) => {
-    item.addEventListener('click', () => {
-
-        document.getElementById("figure").classList.remove('hide');
-        document.getElementById("hide-3").classList.add('hide');
-        setTimeout(() => {
-            document.querySelector('div[data-target="phrase-8"]').classList.remove('hide');
-            setTimeout(() => {
-                document.querySelector('div[data-target="phrase-9"]').classList.remove('hide');
-                setTimeout(() => {
-                    document.querySelector('div[data-target="phrase-10"]').classList.remove('hide');
-                    setTimeout(() => {
-                        document.querySelector('div[data-target="phrase-11"]').classList.remove('hide');
-                        setTimeout(() => {
-                            document.querySelector('div[data-target="phrase-12"]').classList.remove('hide');
-                            setTimeout(() => {
-                                document.querySelector('div[data-target="phrase-13"]').classList.remove('hide');
-                            }, 4000);
-                        }, 8000);
-                    }, 8000);
-                }, 8000);
-            }, 8000);
-        }, 8000)
-    });
-});
-
-noAnswers.forEach((answer) => {
+cancelAnswers.forEach((answer) => {
     answer.addEventListener('click', () => {
-        const section = answer.closest('section');
-
         const farewellSplit = SplitText.create(".modal__link", { type: "words, chars" });
 
         const farewellTl = gsap.timeline();
@@ -85,12 +149,12 @@ noAnswers.forEach((answer) => {
                 duration: 0.6,
                 ease: 'power4.out',
             })
-            .to(section, {
+            .to('.main', {
                 opacity: 0,
                 duration: 0.6,
                 ease: 'power4.out',
             }, '<')
-            .to(noModal, {
+            .to(cancelModal, {
                 autoAlpha: 1,
                 duration: 1.5,
                 ease: 'sine.in'
@@ -117,6 +181,17 @@ noAnswers.forEach((answer) => {
     });
 });
 
-yes.addEventListener('click', () => {
-    yesModal.classList.add('active');
+startBtn.addEventListener('click', () => {
+    gsap.to(window, {
+        duration: .5,
+        scrollTo: { y: '.message', offsetY: 65, },
+        ease: "sine.inOut",
+    });
+    startBtn.style.pointerEvents = 'none';
+    startBtn.nextElementSibling.style.pointerEvents = 'none';
+
+}, { once: true });
+
+acceptBtn.addEventListener('click', () => {
+    acceptModal.classList.add('active');
 })
